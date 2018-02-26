6fe719f7f4d7c4db62690df7bec8dcd39228fef8
He Lin <oxxxxd@gmail.com>
2018-02-26 10:20:34 +0800

    form-field validation and submit.

--- js/index.js
+++ js/index.js
@@ -24,3 +24,40 @@ $( ()=>{
         }
     )
 });
+
+// 提交留言
+$(() => {
+    var $submitBtn = $('.message-submit'),
+        $formField = $(".form-field"),
+        $thxNote = $('.thx-note');
+    
+    $submitBtn.click(function (e) { 
+        e.preventDefault();
+        console.log($formField);
+        var $uname = $formField.find('[name=uname]'),
+            $uemail = $formField.find('[name=uemail]'),
+            $utext = $formField.find('[name=utext]');
+        var isUnameValidated = true, isUemailValidated = true;
+        // console.log(uname, uemail, utext);
+        if (!$uname.val()) {
+            isUnameValidated = false;
+            $uname.next().show();
+        } else {
+            isUnameValidated = true;
+            $uname.next().hide();
+        }
+        if (!$uemail.val()) {
+            isUemailValidated = false;
+            $uemail.next().show();
+        } else {
+            isUemailValidated = true;
+            $uemail.next().hide();
+        }
+
+        if (isUnameValidated && isUemailValidated) {
+            $formField.hide();
+            $thxNote.show();
+        }
+        
+    });
+})
